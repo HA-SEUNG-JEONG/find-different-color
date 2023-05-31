aeee57a352db297266e17f12a863d3023ca92d6b
gktmd652@naver.com <88266129+HA-SEUNG-JEONG@users.noreply.github.com>
2023-05-31 16:12:50 +0900

    🎨 로직 개선 - colorDifference 및 opacity 로그 계속해서 찍히는 현상을 useMemo로 수정

--- src/hooks/useGenerateColor.ts
+++ src/hooks/useGenerateColor.ts
@@ -1,14 +1,19 @@
+import { useMemo } from "react";
+
 const useGenerateColor = (
   stage: number,
   index: number,
   answerIndex: number
 ) => {
-  const baseHue = Math.floor((stage - 1) * 60) % 360;
-  const saturation = 200;
-  const lightness = 30;
-  const opacity = index === answerIndex ? 1 : 0.7;
+  return useMemo(() => {
+    const baseHue = Math.floor((stage - 1) * 60) % 360;
+    const saturation = 200;
+    const lightness = 30;
+    const colorDifference = Math.min(0.2, (stage - 1) * 0.02);
+    const opacity = index === answerIndex ? 0.9 : 0.7 + colorDifference;
 
-  return `rgba(${baseHue}, ${saturation}, ${lightness}, ${opacity})`;
+    return `hsla(${baseHue}, ${saturation}%, ${lightness}%, ${opacity})`;
+  }, [stage, index, answerIndex]);
 };
 
 export default useGenerateColor;
