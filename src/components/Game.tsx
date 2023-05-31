a1225856faf3bed2e0f12b16607ce0f00cadb37a
gktmd652@naver.com <88266129+HA-SEUNG-JEONG@users.noreply.github.com>
2023-05-31 16:26:32 +0900

    🎨 로직 및 스타일 수정 - useRandomSquare hook 적용 - 중복되는 코드 resetGame 함수로 분리 - remainingTime 값에 따라 삼항 연산자로 컴포넌트 렌더링

--- src/components/Game.tsx
+++ src/components/Game.tsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react";
 import styled from "styled-components";
 import Square from "./Square";
+import useRandomSquare from "../hooks/useRandomSquare";
 
 const Game = () => {
   const [stage, setStage] = useState(1);
@@ -8,6 +9,8 @@ const Game = () => {
   const [remainingTime, setRemainingTime] = useState(15);
   const [answerIndex, setAnswerIndex] = useState(0);
   const [gameOver, setGameOver] = useState(false);
+
+  const square = useRandomSquare(stage);
 
   useEffect(() => {
     const timer = setInterval(() => {
@@ -22,77 +25,86 @@ const Game = () => {
     return () => {
       clearInterval(timer);
     };
-  }, [remainingTime]);
+  }, []);
 
-  const generateRandomIndex = (stage: number) => {
-    const gridSize = Math.round((stage + 0.5) / 2) + 1;
-    const totalCells = Math.pow(gridSize, 2);
-    return Math.floor(Math.random() * totalCells);
-  };
-
-  const handleSquareClick = (index: number) => {
-    if (index === answerIndex) {
-      const stageScore = Math.pow(stage, 3) * remainingTime;
-      setScore((prevScore) => prevScore + stageScore);
-      moveToNextStage();
-    } else setRemainingTime((prevTime) => Math.max(prevTime - 3, 0));
+  const resetGame = (square: number) => {
+    setRemainingTime(15);
+    setAnswerIndex(square + 1);
   };
 
   const moveToNextStage = () => {
     setStage((prevStage) => prevStage + 1);
-    setRemainingTime(15);
-    setAnswerIndex(generateRandomIndex(stage + 1));
+    resetGame(square);
+  };
+
+  const handleSquareClick = (index: number) => {
+    //정답인 경우
+    if (index === answerIndex) {
+      const stageScore = Math.pow(stage, 3) * remainingTime;
+      setScore((prevScore) => prevScore + stageScore);
+      moveToNextStage();
+    } //오답인 경우
+    else setRemainingTime((prevTime) => Math.max(prevTime - 3, 0));
   };
 
   const restartGame = () => {
     setStage(1);
     setScore(0);
-    setRemainingTime(15);
-    setAnswerIndex(generateRandomIndex(stage + 1));
+    resetGame(square);
     setGameOver(false);
   };
 
-  if (remainingTime <= 0) {
-    return (
-      <div>
-        <div>Game</div>
+  const randomSquareNumber = Math.pow(Math.round((stage + 0.5) / 2) + 1, 2);
+
+  return remainingTime > 0 ? (
+    <div>
+      <Title>Game</Title>
+      <p>현재 Stage: {stage}</p>
+      <p>현재 Score: {score}</p>
+      <p>남은 시간: {remainingTime}초</p>
+      <Wrapper stage={stage}>
+        {[...Array(randomSquareNumber)].map((_, index) => (
+          <Square
+            key={index}
+            index={index}
+            answerIndex={answerIndex}
+            stage={stage}
+            onClick={handleSquareClick}
+          />
+        ))}
+      </Wrapper>
+    </div>
+  ) : (
+    <>
+      <GameDisplay>
         <p>게임 종료</p>
         <p>최종 stage: {stage}</p>
         <p>누적 score: {score}</p>
-        <button onClick={restartGame}>새로운 게임시작</button>
-      </div>
-    );
-  }
-  return (
-    <div>
-      <p>Stage: {stage}</p>
-      <p>Score: {score}</p>
-      <p>남은 시간: {remainingTime}초</p>
-
-      <Wrapper stage={stage}>
-        {[...Array(Math.pow(Math.round((stage + 0.5) / 2) + 1, 2))].map(
-          (_, index) => (
-            <Square
-              key={index}
-              index={index}
-              answerIndex={answerIndex}
-              stage={stage}
-              onClick={handleSquareClick}
-            />
-          )
-        )}
-      </Wrapper>
-    </div>
+      </GameDisplay>
+      <button onClick={restartGame}>새로운 게임시작</button>
+    </>
   );
 };
+
+const Title = styled.p`
+  font-size: 2rem;
+  font-weight: bold;
+  text-align: left;
+  margin-top: 2rem;
+`;
+
+const GameDisplay = styled.div`
+  width: 18rem;
+  padding-left: 1.25rem;
+`;
 
 const Wrapper = styled.section<{ stage: number }>`
   display: grid;
   grid-template-columns: repeat(
     ${(props) => Math.round((props.stage + 0.5) / 2) + 1},
-    50px
+    3rem
   );
-  gap: 5px;
+  gap: 0.3rem;
 `;
 
 export default Game;
