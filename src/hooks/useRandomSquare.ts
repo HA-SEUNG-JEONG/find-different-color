d1e4a01c7209702c7f4042851b45047e91ba4c56
gktmd652@naver.com <88266129+HA-SEUNG-JEONG@users.noreply.github.com>
2023-05-31 16:10:36 +0900

    🔥 콘솔로그 삭제

--- src/hooks/useRandomSquare.ts
+++ src/hooks/useRandomSquare.ts
@@ -1,8 +1,6 @@
 const useRandomSquare = (stage: number) => {
   const gridSize = Math.round((stage + 0.5) / 2) + 1;
-
   const totalCells = Math.pow(gridSize, 2);
-  console.log(gridSize, totalCells);
   return Math.floor(Math.random() * totalCells);
 };
 
