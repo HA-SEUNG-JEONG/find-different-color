16f92f330187996e8281c8f4e78deb84c0e520fb
gktmd652@naver.com <88266129+HA-SEUNG-JEONG@users.noreply.github.com>
2023-06-04 01:00:15 +0900

    💄 랜덤 컬러 생성 방식 변경

--- src/hooks/useGenerateColor.ts
+++ src/hooks/useGenerateColor.ts
@@ -7,13 +7,12 @@ const useGenerateColor = (
 ) => {
   return useMemo(() => {
     const baseHue = Math.floor((stage - 1) * 60) % 360;
-    const saturation = 200;
-    const lightness = 30;
+    const saturation = 100;
+    const lightness = 30 + Math.min(0.2, (stage - 1) * 0.02);
     const colorDifference = Math.min(0.2, (stage - 1) * 0.02);
     const opacity = index === answerIndex ? 0.9 : 0.7 + colorDifference;
-
-    return `hsla(${baseHue}, ${saturation}%, ${lightness}%, ${opacity})`;
-  }, [stage, index, answerIndex]);
+    return `rgba(${baseHue}, ${saturation}, ${lightness}, ${opacity})`;
+  }, [stage]);
 };
 
 export default useGenerateColor;
