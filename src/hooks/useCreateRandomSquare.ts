ee58c9cdc8fe43a3a62db187a2062b3824a353a9
gktmd652@naver.com <88266129+HA-SEUNG-JEONG@users.noreply.github.com>
2023-06-10 12:11:15 +0900

    🚚 hooks 네이밍 변경

--- src/hooks/useCreateRandomSquare.ts
+++ src/hooks/useCreateRandomSquare.ts
@@ -1,7 +1,12 @@
-const useRandomSquare = (stage: number) => {
+import { useMemo } from "react";
+
+const useCreateRandomSquare = (stage: number) => {
   const gridSize = Math.round((stage + 0.5) / 2) + 1;
   const totalCells = Math.pow(gridSize, 2);
-  return Math.floor(Math.random() * totalCells);
+  return useMemo(() => {
+    const randomIndex = Math.floor(Math.random() * totalCells);
+    return randomIndex;
+  }, [totalCells]);
 };
 
-export default useRandomSquare;
+export default useCreateRandomSquare;
