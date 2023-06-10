7502d4d28bde3ef0d93a355e509055b6034a4b58
gktmd652@naver.com <88266129+HA-SEUNG-JEONG@users.noreply.github.com>
2023-06-10 12:07:36 +0900

    ✅ colorDifference 값 변경

--- src/hooks/useGenerateColor.ts
+++ src/hooks/useGenerateColor.ts
@@ -9,7 +9,7 @@ const useGenerateColor = (
     const baseHue = Math.floor((stage - 1) * 60) % 360;
     const saturation = 100;
     const lightness = 30 + Math.min(0.2, (stage - 1) * 0.02);
-    const colorDifference = Math.min(0.2, (stage - 1) * 0.02);
+    const colorDifference = Math.min(0.2, (stage - 1) * 0.002);
     const opacity = index === answerIndex ? 0.9 : 0.7 + colorDifference;
     return `rgba(${baseHue}, ${saturation}, ${lightness}, ${opacity})`;
   }, [stage]);
