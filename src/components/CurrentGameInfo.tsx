beb090cf9fc9cb6a66375c6b837bea31aec5a9af
gktmd652@naver.com <88266129+HA-SEUNG-JEONG@users.noreply.github.com>
2023-06-19 08:54:48 +0900

    💄 게임 정보 스타일 변경

--- src/components/CurrentGameInfo.tsx
+++ src/components/CurrentGameInfo.tsx
@@ -1,13 +1,21 @@
+import styled from "styled-components";
 import { ScoreProps } from "../data/scoreProps";
 
 const Score = ({ state }: ScoreProps) => {
   return (
-    <>
+    <CurrentInfo>
       <p>현재 Stage: {state.stage}</p>
       <p>현재 Score: {state.score}</p>
       <p>남은 시간: {state.remainingTime}초</p>
-    </>
+    </CurrentInfo>
   );
 };
+
+const CurrentInfo = styled.div`
+  display: flex;
+  width: 25rem;
+  gap: 1rem;
+  font-weight: bold;
+`;
 
 export default Score;
--- src/components/FinalGameInfo.tsx
+++ src/components/FinalGameInfo.tsx
@@ -1,13 +1,21 @@
+import styled from "styled-components";
 import { ScoreProps } from "../data/scoreProps";
 
 const FinalScore = ({ state }: ScoreProps) => {
   return (
-    <>
+    <FinalInfo>
       <p>게임 종료</p>
       <p>최종 stage: {state.stage}</p>
       <p>score: {state.score}</p>
-    </>
+    </FinalInfo>
   );
 };
+
+const FinalInfo = styled.div`
+  display: flex;
+  width: 20rem;
+  gap: 1rem;
+  font-weight: bold;
+`;
 
 export default FinalScore;
