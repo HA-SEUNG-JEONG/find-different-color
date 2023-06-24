b1496c78a36a1a21453ba739d2c04654bdf5f15f
gktmd652@naver.com <88266129+HA-SEUNG-JEONG@users.noreply.github.com>
2023-06-24 11:57:04 +0900

    🔥 hooks 폴더 삭제

--- src/hooks/useCreateRandomSquare.ts
+++
@@ -1,7 +0,0 @@
-const useCreateRandomSquare = (stage: number) => {
-  const gridSize = Math.round((stage + 0.5) / 2) + 1;
-  const totalCells = Math.pow(gridSize, 2);
-  return Math.floor(Math.random() * totalCells);
-};
-
-export default useCreateRandomSquare;
