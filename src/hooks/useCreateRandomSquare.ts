2ce74260ac61834e7ec67eb2d5ed7ad0d443bbd3
gktmd652@naver.com <88266129+HA-SEUNG-JEONG@users.noreply.github.com>
2023-06-24 11:38:31 +0900

    🔥 useMemo 삭제

--- src/hooks/useCreateRandomSquare.ts
+++ src/hooks/useCreateRandomSquare.ts
@@ -1,12 +1,7 @@
-import { useMemo } from "react";
-
 const useCreateRandomSquare = (stage: number) => {
   const gridSize = Math.round((stage + 0.5) / 2) + 1;
   const totalCells = Math.pow(gridSize, 2);
-  return useMemo(() => {
-    const randomIndex = Math.floor(Math.random() * totalCells);
-    return randomIndex;
-  }, [totalCells]);
+  return Math.floor(Math.random() * totalCells);
 };
 
 export default useCreateRandomSquare;
--- src/hooks/useGenerateColor.ts
+++ src/hooks/useGenerateColor.ts
@@ -1,18 +1,14 @@
-import { useMemo } from "react";
-
 const useGenerateColor = (
   stage: number,
   index: number,
   answerIndex: number
 ) => {
-  return useMemo(() => {
-    const baseHue = Math.floor((stage - 1) * 60) % 360;
-    const saturation = 100;
-    const lightness = 30 + Math.min(0.2, (stage - 1) * 0.02);
-    const colorDifference = Math.min(0.2, (stage - 1) * 0.002);
-    const opacity = index === answerIndex ? 0.9 : 0.7 + colorDifference;
-    return `rgba(${baseHue}, ${saturation}, ${lightness}, ${opacity})`;
-  }, [stage]);
+  const baseHue = Math.floor((stage - 1) * 60) % 360;
+  const saturation = 100;
+  const lightness = 30 + Math.min(0.2, (stage - 1) * 0.02);
+  const colorDifference = Math.min(0.2, (stage - 1) * 0.002);
+  const opacity = index === answerIndex ? 0.9 : 0.7 + colorDifference;
+  return `rgba(${baseHue}, ${saturation}, ${lightness}, ${opacity})`;
 };
 
 export default useGenerateColor;
